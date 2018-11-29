dd95f34baf81d6aceaf00ce356aec300dfbe1ae3
Daniel Blanco <daniel@bmya.cl>
2018-11-29 10:59:28 -0300

    [IMP] fixes #277 (2) www.bmya.cl/tickets

--- account_financial_report_qweb/report/general_ledger.py
+++ account_financial_report_qweb/report/general_ledger.py
@@ -1143,7 +1143,17 @@ SELECT
     '""" + _('No partner allocated') + """' AS partner,
             """
         query_inject_move_line += """
-    CONCAT_WS(' - ', NULLIF(ml.ref, ''), NULLIF(ml.name, '')) AS label,
+    /* CONCAT_WS(
+        ' - ', NULLIF(ml.ref, ''), NULLIF(ml.name, '')
+    ) AS label, */
+    CASE
+        WHEN NULLIF(ml.ref, '') THEN ml.ref
+        ELSE
+            CASE
+                WHEN NULLIF(ml.name, '') THEN ml.name
+                ELSE '--'
+            END
+    END AS label,
     aa.name AS cost_center,
     fr.name AS matching_number,
     ml.debit,
